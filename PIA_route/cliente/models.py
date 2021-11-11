3d57a11e224220599526a347cba04c7ee27aa03c
danymessi1000@gmail.com <danymessi1000@gmail.com>
2021-11-10 22:32:52 -0600

    cambio

--- PIA_route/cliente/models.py
+++ PIA_route/cliente/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Cliente (models.Model):
-    rfc=models.CharField(max_length=50)
-    nombre=models.CharField(max_length=50)
-    edad=models.FloatField()
+    matricula=models.FloatField(default=0)
+    nombre=models.CharField(max_length=50,default="")
+    apellidoPaterno=models.CharField(max_length=50,default="")
+    apellidoMaterno=models.CharField(max_length=50,default="")
+    edad=models.FloatField(default=0)
 
+    
 
     def __str__(self):
-        return self.nombre
+        
+        return str(self.matricula)
--- PIA_route/empleado/models.py
+++ PIA_route/empleado/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Empleado (models.Model):
+    matricula=models.FloatField(default=0)
     rfc=models.CharField(max_length=50)
     nombre=models.CharField(max_length=50)
     direccion=models.TextField()
